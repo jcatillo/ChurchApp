38e69d0a058f3957a5d8259ea69b3a7e93070de3
jcatillo <jcatillo1121@gmail.com>
2025-05-15 19:56:23 +0800

    try commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, ()=>{
-    console.log('Server started at http://localhost:5000');
+app.get("/", (req, res)=>{
+    res.send("Otin123");
+});
+
+app.listen(5001, ()=>{
+    connectDb();
+    console.log("Server started at http://localhost:5001");
 });
